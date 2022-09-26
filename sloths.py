85acc1f0bbac41513e97e03cec3fd87d56948134
juliwithoutthee <jnboucree@gmail.com>
2022-09-26 15:30:48 +0900

    Prints the length of species that are three toed

--- sloths.py
+++ sloths.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pexpect import split_command_line
 
 
 # data frame 
@@ -8,6 +9,9 @@ df = pd.read_csv('sloth_data.csv')
 print(df.head())
 
 specie = df['specie'] 
-print(specie)
+# print(specie)
 
-critically_endangered = df["critically_endangered"]
+specie_three_toed = df[(df["specie"] == "three_toed")]
+print(len(specie_three_toed))
+
+# critically_endangered = df["critically_endangered"]
