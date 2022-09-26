4983cac8526e05f05f8e3d8f27a0962bd75ac24d
juliwithoutthee <jnboucree@gmail.com>
2022-09-26 17:16:29 +0900

    Fixes variable for critically endangered sloths

--- sloths.py
+++ sloths.py
@@ -17,8 +17,8 @@ print(len(specie_three_toed))
 specie_two_toed = df[(df["specie"] == "two_toed")]
 print(len(specie_two_toed))
 
-# critically_endangered = df["critically_endangered"]  # Fix later
-
+critically_endangered = df[(df["endangered"] == "critically_endangered")] 
+print(len(critically_endangered))
 
 # Filters and creates a new csv file 
 specie_three_toed.to_csv("specie-three-toed.csv")
