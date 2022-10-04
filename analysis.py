9047bef574ff7ec9b608f66c1b1935751526163e
juliwithoutthee <jnboucree@gmail.com>
2022-10-04 16:32:50 +0900

    Adds imports

--- analysis.py
+++ analysis.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
+
 
 df = pd.read_csv('sloth_data.csv')
 
 # Gives descriptive statistics 
 print(df.describe())
+
+plt.plot([1,2,3], [2,4,6])
